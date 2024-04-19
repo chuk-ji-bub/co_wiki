b541ed3f493ab15a1e36cc744921df4f6a1c9b65
slowtree999 <zxcv828133@gmail.com>
2024-04-19 15:02:17 +0900

    1

---
+++ frontend/src/pages/main.tsx
@@ -0,0 +1,5 @@
+import Header from '../components/header';
+import aside_content from '../components/aside_content';
+import main_content from '../components/main_content';
+import side_mark from '../components/side_mark';
+import side_memo from '../components/side_memo';
