1314cef95ef5391b08309ecdd5132f18a0c3017f
Sinsu78 <pqpq7838@gmail.com>
2024-04-19 15:26:07 +0900

    asdf
    asdf

---
+++ frontend/src/components/middle.tsx
@@ -0,0 +1,16 @@
+// 메인페이지에 선택하면 나올 텍스트박스
+
+// MiddleBox.tsx
+import React from 'react';
+//import './MiddleBox.css';   스타일링을 위한 별도의 CSS 파일을 생성할 수 있습니다.
+
+const Middle: React.FC = () => {
+  return (
+    <div className="middle-box">
+      {/* 여기에 내용을 추가하세요 */}
+      <p>JavaScript 내용을 여기에 추가하세요</p>
+    </div>
+  );
+};
+
+export default Middle;
--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -1,5 +1,14 @@
-import Header from '../components/header';
-import aside_content from '../components/aside_content';
-import main_content from '../components/main_content';
-import side_mark from '../components/side_mark';
-import side_memo from '../components/side_memo';
+import React from 'react';
+import MiddleBox from '../components/middle'; // MiddleBox 컴포넌트를 불러옵니다.
+
+const Main: React.FC = () => {
+  return (
+    <div className="main-container">
+      {/* 다른 컴포넌트 */}
+      <MiddleBox /> {/* MiddleBox 컴포넌트를 렌더링합니다. */}
+      {/* 다른 컴포넌트 */}
+    </div>
+  );
+};
+
+export default Main;
