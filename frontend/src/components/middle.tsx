6daaf74a0b2788dc976e1a83115f2ad005472cc9
slowtree999 <zxcv828133@gmail.com>
2024-04-19 15:31:09 +0900

    1

--- frontend/src/components/middle.tsx
+++ frontend/src/components/middle.tsx
@@ -1,9 +1,4 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-// 메인페이지에 선택하면 나올 텍스트박스
-=======
->>>>>>> Stashed changes
+
 // 메인페이지에 선택하면 나올 텍스트박스
 
 // MiddleBox.tsx
@@ -19,9 +14,5 @@ const Middle: React.FC = () => {
   );
 };
 
-<<<<<<< Updated upstream
 export default Middle;
-=======
-export default Middle;
->>>>>>> 1314cef95ef5391b08309ecdd5132f18a0c3017f
->>>>>>> Stashed changes
+
--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -1,36 +1,5 @@
 import React from 'react';
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 
-import Header from '../components/Header';
-import left from '../components/left';
-import Main_content from '../components/middle';
-import Side_mark from '../components/rightup';
-import Side_memo from '../components/rightdown';
-
-// 메인 페이지 컴포넌트
-const Main: React.FC = () => {
-
-  
-    return (
-      <div>
-        <header>
-          <Header/>
-        </header>
-        <main>
-          <div style={{ left: '10px' }}>
-            <left/>
-          </div>
-        </main>
-        
-      </div>
-    );
-  };
-  
-  export default Main;
-=======
->>>>>>> Stashed changes
 import MiddleBox from '../components/middle'; // MiddleBox 컴포넌트를 불러옵니다.
 
 const Main: React.FC = () => {
@@ -43,9 +12,7 @@ const Main: React.FC = () => {
   );
 };
 
-<<<<<<< Updated upstream
 export default Main;
-=======
-export default Main;
->>>>>>> 1314cef95ef5391b08309ecdd5132f18a0c3017f
->>>>>>> Stashed changes
+
+
+
