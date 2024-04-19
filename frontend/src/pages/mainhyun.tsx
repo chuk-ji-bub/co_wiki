d927cb9282d136a5ec9f1a039e38668764a20d34
Sinsu78 <pqpq7838@gmail.com>
2024-04-19 15:41:09 +0900

    1
    1

---
+++ frontend/src/pages/mainhyun.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import MiddleBox from '../components/middle'; // MiddleBox 컴포넌트를 불러옵니다.
+
+const Main: React.FC = () => {
+  return (
+    <div className="main-container">
+      {/* 다른 컴포넌트 */}
+      <MiddleBox /> {/* MiddleBox 컴포넌트를 렌더링합니다. */}
+      {/* 다른 컴포넌트 */}
+    </div>
+  );
+};
+
+export default Main;
+
+
+
