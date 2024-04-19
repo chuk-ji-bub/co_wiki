da27402bd09c722d2b75c8f61edbef78e3b34939
slowtree999 <zxcv828133@gmail.com>
2024-04-19 10:39:25 +0900

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div>
+        {/* 여기에 네비게이션 바나 공통 레이아웃 컴포넌트를 추가할 수 있습니다 */}
+        <Routes>
+          {/* <Route path="/" element={<Main />} /> */}
+          
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
