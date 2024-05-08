0bcc5be3c227e0ab3d6d0cde5278f9c01c8c0081
slowtree999 <zxcv828133@gmail.com>
2024-05-08 20:12:37 +0900

    1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './pages/main';
+
 import Mainhyun from './pages/mainhyun';
-import Maininsung from './pages/maininsung';
+import Maininsung from '../src/pages/login/index';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div>
-        {/* 여기에 네비게이션 바나 공통 레이아웃 컴포넌트를 추가할 수 있습니다 */}
+        
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/1" element={<Mainhyun/>} />
---
+++ frontend/src/pages/main/index.tsx
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react';
+import './style.css';
+import Header from '../../components/Header/Header';
+
+
+
+
+const Main: React.FC = () => {
+
+
+
+  
+  return (
+    <div className="main-container">
+      
+      <Header /> 
+      
+    </div>
+  );
+};
+
+export default Main;
