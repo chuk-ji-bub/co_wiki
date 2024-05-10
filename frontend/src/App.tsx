6b04715a915ae68a26f092fd7c0bb2fb3257508a
slowtree999 <zxcv828133@gmail.com>
2024-05-10 11:14:09 +0900

    11

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { UserProvider } from './components/UserContext/UserContext';
+import { UserProvider, useUser } from './components/UserContext/UserContext';
+import { Navigate } from 'react-router-dom';
 import Main from '../src/pages/main/index';
 import Login from './pages/login/index';
 import Signup from './pages/signup/index';
-
+import Userpage from './pages/userpage/index';
 import Mainhyun from './pages/mainhyun';
 
 
-const App: React.FC = () => {
+function App() {
   
   const storedUserName = localStorage.getItem('userName');
 
@@ -22,10 +23,21 @@ const App: React.FC = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
 
+          <Route path="/userpage" element={<UserpageRoute />} />
         </Routes>
       </Router>
     </UserProvider>
   );
 };
+
+function UserpageRoute() {
+  const { userName } = useUser();
+
+  if (userName) {
+    return <Userpage />;
+  } else {
+    return <Navigate to="/login" replace />;
+  }
+}
 
 export default App;
---
+++ frontend/src/pages/userpage/index.tsx
@@ -0,0 +1,19 @@
+//main 페이지
+
+import React, { useRef } from 'react';
+import Header from '../../components/Header/Header';
+
+
+
+
+const Userpage: React.FC = () => {
+
+
+  return (
+    <div className="Header">
+      <Header /> 
+    </div>
+  );
+};
+
+export default Userpage;
