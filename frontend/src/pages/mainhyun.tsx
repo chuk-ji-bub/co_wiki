02f4fd9612391d6cb44920e012c6d831d78e7ff3
Sinsu78 <pqpq7838@gmail.com>
2024-05-17 16:15:08 +0900

    11

--- frontend/src/pages/mainhyun.tsx
+++ frontend/src/pages/mainhyun.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import MiddleBox from '../components/middle/middle'; // MiddleBox 컴포넌트를 불러옵니다.
-//import LeftBox from '../components/left'; // LeftBox 컴포넌트를 불러옵니다.
+import LeftBox from '../components/left/left'; // LeftBox 컴포넌트를 불러옵니다.
+import RightBox from '../components/right/right'; // RightBox 컴포넌트를 불러옵니다.
+import Header from '../components/Header/Header';
 //import Main from './main';
 
 
@@ -9,7 +11,10 @@ const App: React.FC = () => {
     return (
       <div className="main-container">
         {/* MiddleBox와 LeftBox 컴포넌트를 모두 렌더링합니다. */}
+        <Header/>
         <MiddleBox />
+        {/* <LeftBox/>*/}
+        <RightBox/>
       </div>
     );
   };
